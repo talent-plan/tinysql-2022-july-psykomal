51c742252d75408c3ad3cfd83d1aedf7e768f9c7
psykomal <bkomalsai@gmail.com>
2023-10-02 19:39:12 +0530

    updateVersionAndTableInfo

--- ddl/table.go
+++ ddl/table.go
@@ -15,6 +15,7 @@ package ddl
 
 import (
 	"fmt"
+
 	"github.com/pingcap/errors"
 	"github.com/pingcap/failpoint"
 	"github.com/pingcap/tidb/infoschema"
@@ -376,6 +377,20 @@ func updateVersionAndTableInfoWithCheck(t *meta.Meta, job *model.Job, tblInfo *m
 func updateVersionAndTableInfo(t *meta.Meta, job *model.Job, tblInfo *model.TableInfo, shouldUpdateVer bool) (
 	ver int64, err error) {
 	// TODO complete this function.
+
+	ver = int64(tblInfo.Version)
+
+	// 1. Update schema version
+	if shouldUpdateVer {
+		ver, err = updateSchemaVersion(t, job)
+		if err != nil {
+			return ver, errors.Trace(err)
+		}
+	}
+
+	tblInfo.UpdateTS = t.StartTS
+
+	err = t.UpdateTable(job.SchemaID, tblInfo)
 
 	return ver, errors.Trace(err)
 }
